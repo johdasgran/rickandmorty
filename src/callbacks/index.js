04a341fd18ee03fc5a3ec95e605fe7d553fe6e36
Johdasgran <dampig14@gmail.com>
2022-01-18 19:46:30 -0500

    callback

--- src/callbacks/index.js
+++ src/callbacks/index.js
@@ -1,9 +1,31 @@
 function sum(n1, n2){
-    return n1 + n2;
+    return n1 + n2 + " desde sum";
 }
 
-function calc(n1, n2, callback) {
-    return callback(n1, n2);
+function resta(n1, n2, n3) {
+    return (n1 - n2)*n3 + " desde resta"
 }
 
-console.log(calc(3, 5, sum));
+function calc(n1, n2, n3, callback) {
+    console.log("callback----")
+    return callback(n1, n2, n3);
+}
+
+console.log(calc(4, 8, 2, resta));
+
+
+/**/
+
+function date(callback) {
+    console.log(new Date, "firts");
+    setTimeout(function () {
+        let fecha = new Date;
+        callback(fecha);
+    }, 3000)
+}
+
+function printDate (dateNow) {
+    console.log(dateNow, "before 3 seconds");
+}
+
+date(printDate);
