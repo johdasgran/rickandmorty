1d271acfd961621dec6781f85934566ac07d5efa
Johdasgran <dampig14@gmail.com>
2022-01-20 13:40:51 -0500

    Albert Einstein

--- src/callbacks/challenge.js
+++ src/callbacks/challenge.js
@@ -21,7 +21,8 @@ function fetchInfo(url_api, callback) {
 
 fetchInfo(API, function (error1, data1){
     if(error1) return console.log(error1);
-    fetchInfo(API + data1.results[0].id, function (error2, data2){
+    // console.log(data1);
+    fetchInfo(API + data1.results[10].id, function (error2, data2){
         if(error2) return console.log(error2);
         fetchInfo(data2.origin.url, function(error3, data3) {
             if(error3) return console.log(error3);
