d21fc6dc610331034d33aa9d1c9417ad0ab658e0
Johdasgran <dampig14@gmail.com>
2022-01-22 19:00:25 -0500

    update async await

--- src/async-await/index.js
+++ src/async-await/index.js
@@ -1,3 +1,18 @@
 const fetchInfo = require("../utils/fetchInfo");
 
 const API = "https://rickandmortyapi.com/api/character/";
+
+const showData = async (url_api) => {
+  try {
+    const data = await fetchInfo(url_api);
+    const character = await fetchInfo(`${url_api}${data.results[0].id}`);
+    const origin = await fetchInfo(character.origin.url);
+    console.log(data.info.count);
+    console.log(character.name);
+    console.log(origin.dimension);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+showData(API);
