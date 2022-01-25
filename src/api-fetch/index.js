a8b856844c71bd241ef128eba51714e9a6b915d1
Johdasgran <dampig14@gmail.com>
2022-01-25 13:05:40 -0500

    create element DOM

--- src/api-fetch/index.js
+++ src/api-fetch/index.js
@@ -75,37 +75,53 @@ const fetch_data = async () => {
     card_text.className = "container-text";
     container_card.appendChild(card_text);
 
+    // Card-title
+    let card_title = document.createElement("div");
+    card_title.className = "card-title";
+    card_text.appendChild(card_title);
+
     // Title
     let title_card = document.createElement("h2");
     title_card.textContent = name;
-    card_text.appendChild(title_card);
+    card_title.appendChild(title_card);
 
     // Status tag "p" container
     let status_card = document.createElement("p");
     status_card.textContent = status;
-    card_text.appendChild(status_card);
+    card_title.appendChild(status_card);
     // Status tag "span" child
     let alive_or_dead = document.createElement("span");
     alive_or_dead.className = "status";
 
     if (status == "Alive") {
       alive_or_dead.textContent = "Status: 🟢 ";
-      status_card.textContent = status;
     } else if (status == "Dead") {
       alive_or_dead.textContent = "Status: 🔴 ";
     } else {
       alive_or_dead.textContent = "Status: ⚪ ";
     }
     status_card.insertAdjacentElement("afterbegin", alive_or_dead);
+
+    // Species tag "p" container
+    let species_card = document.createElement("p");
+    species_card.textContent = species;
+    card_title.appendChild(species_card);
+    // Status tag "span" child
+    let class_species = document.createElement("span");
+    class_species.textContent = "Species: ";
+    class_species.className = "status";
+    species_card.insertAdjacentElement("afterbegin", class_species);
+
+    // Species tag "p" container
+    let gender_card = document.createElement("p");
+    gender_card.textContent = gender;
+    card_title.appendChild(gender_card);
+    // Status tag "span" child
+    let class_gender = document.createElement("span");
+    class_gender.textContent = "Gender: ";
+    class_gender.className = "status";
+    gender_card.insertAdjacentElement("afterbegin", class_gender);
   }
-
-
-
-
-
-
-
-
 
   addCard();
 };
